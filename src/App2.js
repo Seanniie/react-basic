49163840b2de464371e1bc7233da3cb298beab3b
Seannie <shlim4915@gmail.com>
2022-07-01 10:19:33 +0900

    effect

--- src/App2.js
+++ src/App2.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App2(){
+    const [count,setCount]   = useState(0);
+    const [seaniee,setSeanie]   = useState(0);
+    useEffect(()=>{
+        console.log(count);
+    },[count])
+
+    function increment() {
+        setCount(count+1);
+    }
     return(
         <div className="App">
             <h1>Seannie Coder</h1>
+            <button onClick={increment}>Click</button>
+            <button onClick={()=>setSeanie(seaniee+1)}>Click</button>
         </div>
     )
 }
