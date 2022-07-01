e12a6e063422ed84895b3c02dd95488bdb54f3f0
Seannie <shlim4915@gmail.com>
2022-07-01 10:55:14 +0900

    props

--- src/App3.js
+++ src/App3.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 
 function App2(){
-     return(
+    const [buttonName, setButtonName] = useState('클릭');
+    function clickButton(){
+        setButtonName('Click');
+    }
+    return(
         <div className="App">
             <h1>Seannie Coder</h1>
+            <Counter click = "click1"></Counter>
+            <Counter click = {buttonName}></Counter>
             <Counter></Counter>
-            <Counter></Counter>
-            <Counter></Counter>
+            <button onClick={clickButton}>Click</button>
         </div>
-    )
+    );
 }
 
 export default App2;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,19 @@
 import React,{ useState } from "react";
 
-function Counter() {
+function Counter(props) {
     const [count,setCount] = useState(0);
 
     function increment() {
         setCount(count+1);
     };
 
+    const clickString = props.click || 'Click';
+
     return(
         <div className="App">
-            <button onClick={increment}>Click {count}</button>
+            <button onClick={increment}>
+                {clickString} {count}
+            </button>
         </div>
     )
 };
