931c2b76145a7ab962b032112a0ab4bb9898f385
ImSeunghyeon <shlim4915@gmail.com>
2022-11-04 14:32:09 +0900

    app6 추가 추후 학습 예정

---
+++ src/App6.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import routes from "./routes";
+import {
+
+    BrowserRouter as Router,
+    Switch,
+    Route
+    
+} from 'react-router-dom';
+
+
+function App6(){
+
+    return(
+        <></>
+    );
+}
+
+export default App6;
--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import './index.css';
 //import App2 from './App2';
 //import App3 from './App3';
 //import App4 from './App4';
-import App5 from './App5';
+import App6 from './App6';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App5 />
+  <App6 />
 );
 
 reportWebVitals();
